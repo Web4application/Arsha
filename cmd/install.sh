43f9464b934895f1bb1129f6ac14217bb7380d8b
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-01-20 20:54:38 +0100

    Create install.sh

---
+++ cmd/install.sh
@@ -0,0 +1,15 @@
+git remote add origin git@github.com:Web4application/Arsha.git
+git branch -M main
+git push -u origin main
+pip install SpeechRecognition pyttsx3 pyaudio
+pip install pipwin
+pipwin install pyaudio
+brew install portaudio
+pip install pyaudio
+sudo apt install portaudio19-dev
+pip install pyaudio
+asha/
+├── asha.py          # main brain
+├── memory.json      # local memory
+├── commands.py      # command logic
+└── voice.py         # speech in/out
